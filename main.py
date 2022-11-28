75b644b5854b0bddd28fcd76d9a260305882cb6a
Stefan Stefanache <stefanstefanache@protonmail.com>
2022-11-28 18:16:42 +0200

    simplify grammar class. change parse format and add attrs+typing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from grammar import Grammar
 
 
 def print_grammar_menu():
-    print("Menu grammar")
+    print("=== GRAMMAR ===\n")
     print("1 - Read grammar from file")
     print("2 - Print set of non-terminals")
     print("3 - Print set of terminals")
@@ -12,36 +12,41 @@ def print_grammar_menu():
     print("5 - Print set of productions for a given non-terminal")
     print("6 - Check if grammar is cfg")
     print("7 - Exit")
+    print()
 
 
 if __name__ == '__main__':
-    grammar = Grammar("g1.txt")
+    grammar = Grammar.get_grammar_from_file("g1.txt")
     done = False
     while not done:
         print_grammar_menu()
-        option = input("Choose an option:")
+        option = input("Choose an option: ")
         if option == "1":
-            file = input("Write file name:")
+            file = input("File name: ")
             if os.path.exists(file):
-                grammar.read_input_file(file)
+                grammar = Grammar.get_grammar_from_file(file)
                 print("Grammar file read successfully.\n")
             else:
-                print("File does not exist!")
+                print("File does not exist!\n")
         elif option == "2":
-            print(grammar.get_set_non_terminals_str())
+            print(grammar.nonterminals)
         elif option == "3":
-            print(grammar.get_set_terminals_str())
+            print(grammar.terminals)
         elif option == "4":
-            print(grammar.get_set_productions_str())
+            print(grammar.productions)
         elif option == "5":
-            terminal = input("Enter a terminal:")
+            terminal = input("Non-terminal: ")
             try:
-                print(grammar.get_set_productions_for_non_terminal_str(terminal))
+                print(grammar.get_productions_for_nonterminal(terminal))
+                print()
             except Exception as e:
                 print(e)
         elif option == "6":
-            print("\n")
+            if grammar.verify_CFG():
+                print("The given grammar is a cfg.\n")
+            else:
+                print("The given grammar is not a cfg.\n")
         elif option == "7":
             done = True
         else:
-            print("This option does not exist!")
+            print("This option does not exist!\n")
